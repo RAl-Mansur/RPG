7d8a14b28bc7495314594f56ede092c49d6d27d0
Ridwan <ridwan.al.mansur@hotmail.com>
2016-02-03 19:39:17 +0000

    Initial Commit

---
+++ RPG(oop)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPG(oop)
+//
+//  Created by Ridwan Al-Mansur on 03/02/2016.
+//  Copyright © 2016 Ridwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
